2733608f7e18a50e53a22e09fcc4284d18bddaed
Dmitry Menshikov <dmitriy.menshikov@auroraglobal.com>
2017-08-18 12:13:31 +0300

    StreamsInfo _adjustAspectRatio test refactoring (refs #18200)

--- tests/StreamsInfo/Unit/_adjustAspectRatio.data.js
+++ tests/StreamsInfo/Unit/_adjustAspectRatio.data.js
@@ -8,19 +8,44 @@ const invalidParams = [
         description: 'height param is invalid',
         data       : [{sample_aspect_ratio: '0:1', display_aspect_ratio: '10:1', width: 10, height: 'N/A'}],
         errorMsg   : 'height field has invalid value.'
-    }
+    },
+    {
+        description: 'height param is invalid',
+        data       : [{sample_aspect_ratio: '10:1', display_aspect_ratio: '0:1', width: 'N/A', height: 10}],
+        errorMsg   : 'height field has invalid value.'
+    },
+    {
+        description: 'height param is invalid',
+        data       : [{sample_aspect_ratio: '10:1', display_aspect_ratio: '0:1', width: 10, height: 'N/A'}],
+        errorMsg   : 'height field has invalid value.'
+    },
 ];
 
 const validParams = [
     {
         description: 'sample_aspect_ratio param is invalid',
+        data       : [],
+        res        : []
+    },
+    {
+        description: 'sample_aspect_ratio param is invalid',
         data       : [{sample_aspect_ratio: '0:1', display_aspect_ratio: '200:100', width: 10, height: 4}],
-        res        : {sample_aspect_ratio: '1:1', display_aspect_ratio: '5:2'}
+        res        : [{sample_aspect_ratio: '1:1', display_aspect_ratio: '5:2', width: 10, height: 4}]
     },
     {
         description: 'display_aspect_ratio param is invalid',
         data       : [{sample_aspect_ratio: '200:100', display_aspect_ratio: '0:1', width: 20, height: 10}],
-        res        : {sample_aspect_ratio: '1:1', display_aspect_ratio: '2:1'}
+        res        : [{sample_aspect_ratio: '1:1', display_aspect_ratio: '2:1', width: 20, height: 10}]
+    },
+    {
+        description: 'sample_aspect_ratio param is invalid',
+        data       : [{sample_aspect_ratio: 'N/A', display_aspect_ratio: '200:100', width: 10, height: 4}],
+        res        : [{sample_aspect_ratio: '1:1', display_aspect_ratio: '5:2', width: 10, height: 4}]
+    },
+    {
+        description: 'display_aspect_ratio param is invalid',
+        data       : [{sample_aspect_ratio: '200:100', display_aspect_ratio: 'N/A', width: 20, height: 10}],
+        res        : [{sample_aspect_ratio: '1:1', display_aspect_ratio: '2:1', width: 20, height: 10}]
     }
 ];
 
