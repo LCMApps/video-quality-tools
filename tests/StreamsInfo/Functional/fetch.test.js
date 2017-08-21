a95ce0c1f05695388ce111cce228019e9d12d68b
Eduard Bondarenko <eduard.bondarenko@auroraglobal.com>
2017-08-21 11:09:48 +0300

    Update tests, rewrite from BDD to TDD. Find free port.

--- tests/StreamsInfo/Functional/fetch.test.js
+++ tests/StreamsInfo/Functional/fetch.test.js
@@ -5,50 +5,68 @@ const {spawn} = require('child_process');
 
 const {assert} = require('chai');
 
+const getPort = require('get-port');
+
 const StreamsInfo = require('StreamsInfo');
 
 const {StreamsInfoError} = require('Errors');
 
 describe('StreamsInfo::fetch functional', () => {
 
-    const testFile  = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
-    const streamURL = 'http://localhost:8888';
+    assert(process.env.FFPROBE, 'Specify path for ffprobe using FFPROBE env var');
+    assert(process.env.FFMPEG, 'Specify path for ffmpeg using FFMPEG env var');
 
-    let streamsInfo = new StreamsInfo({
-        ffprobePath : process.env.FFPROBE_PATH,
-        timeoutInSec: 1,
-    }, streamURL);
+    const {FFPROBE, FFMPEG} = process.env;
+    const testFile          = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
 
-    it('fetch streams info from inactive stream', function () {
-        this.timeout(5 * 1000);
+    let streamUrl;
+    let streamsInfo;
 
-        return streamsInfo.fetch()
-            .then(assert.isNotDefined)
-            .catch(err => {
-                assert.instanceOf(err, StreamsInfoError);
+    before(async () => {
+        const port = await getPort();
 
-                assert(err.message);
-            });
+        streamUrl = `http://localhost:${port}`;
+
+        streamsInfo = new StreamsInfo({
+            ffprobePath : FFPROBE,
+            timeoutInSec: 1,
+        }, streamUrl);
     });
 
-    // TODO: process.env.FFMPEG
-    // TODO: kill ffmpeg
+    it('fetch streams info from inactive stream', async function () {
+        this.timeout(5 * 1000);
+
+        try {
+            await streamsInfo.fetch();
+
+            assert.fail('fetch method must throw an error, why are you still here ?');
+        } catch (err) {
+            assert.instanceOf(err, StreamsInfoError);
+
+            assert(err.message);
+        }
+    });
+
     it('fetch streams info from active stream', function (done) {
         this.timeout(5 * 1000);
 
-        let command  = `ffmpeg -re -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamURL}`.split(' ');
+        let command  = `${FFMPEG} -re -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamUrl}`.split(' ');
         const ffmpeg = spawn(command[0], command.slice(1));
 
         // we listen stderr cuz we rely on stderr banner output
         ffmpeg.stderr.once('data', async () => {
-            const info = await streamsInfo.fetch();
+            try {
+                const info = await streamsInfo.fetch();
 
-            assert.isNotEmpty(info.videos);
-            assert.isNotEmpty(info.audios);
+                assert.isObject(info);
 
-            ffmpeg.kill();
+                assert.isNotEmpty(info.videos);
+                assert.isNotEmpty(info.audios);
+            } finally {
+                ffmpeg.kill();
 
-            done();
+                done();
+            }
         });
     });
 });
