52020857c420f612f9e48ec8570d61f6b97ee802
Eduard Bondarenko <eduardbcom@gmail.com>
2017-08-21 12:23:25 +0300

    Improve func test for the StreamsInfo::fetch method

--- tests/Functional/StreamsInfo/fetch.test.js
+++ tests/Functional/StreamsInfo/fetch.test.js
@@ -11,13 +11,13 @@ const StreamsInfo = require('src/StreamsInfo');
 
 const {StreamsInfoError} = require('src/Errors');
 
-describe('StreamsInfo::fetch functional', () => {
+assert(process.env.FFPROBE, 'Specify path for ffprobe via FFPROBE env var');
+assert(process.env.FFMPEG, 'Specify path for ffmpeg via FFMPEG env var');
 
-    assert(process.env.FFPROBE, 'Specify path for ffprobe via FFPROBE env var');
-    assert(process.env.FFMPEG, 'Specify path for ffmpeg via FFMPEG env var');
+const {FFPROBE, FFMPEG} = process.env;
+const testFile          = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
 
-    const {FFPROBE, FFMPEG} = process.env;
-    const testFile          = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
+describe('StreamsInfo::fetch, fetch streams info from inactive stream', () => {
 
     let streamUrl;
     let streamsInfo;
@@ -47,28 +47,47 @@ describe('StreamsInfo::fetch functional', () => {
         }
     });
 
+});
+
+describe('StreamsInfo::fetch, fetch streams info from active stream', () => {
+
+    let streamUrl;
+    let streamsInfo;
+    let ffmpeg;
+
+    before(async () => {
+        const port = await getPort();
+
+        streamUrl = `http://localhost:${port}`;
+
+        streamsInfo = new StreamsInfo({
+            ffprobePath : FFPROBE,
+            timeoutInSec: 1,
+        }, streamUrl);
+
+        let command = `${FFMPEG} -re -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamUrl}`.split(' ');
+
+        ffmpeg = spawn(command[0], command.slice(1));
+    });
+
+    after(() => {
+        ffmpeg.kill('SIGKILL');
+    });
+
     it('fetch streams info from active stream', function (done) {
         this.timeout(5 * 1000);
 
-        let command  = `${FFMPEG} -re -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamUrl}`.split(' ');
-        const ffmpeg = spawn(command[0], command.slice(1));
-
         // we listen stderr cuz we rely on stderr banner output
         ffmpeg.stderr.once('data', async () => {
-            try {
-                const info = await streamsInfo.fetch();
+            const info = await streamsInfo.fetch();
 
-                assert.isObject(info);
+            assert.isObject(info);
 
-                assert.isNotEmpty(info.videos);
-                assert.isNotEmpty(info.audios);
-            } catch (err) {
-                assert.ifError(err);
-            } finally {
-                ffmpeg.kill();
+            assert.isNotEmpty(info.videos);
+            assert.isNotEmpty(info.audios);
 
-                done();
-            }
+            done();
         });
     });
+
 });
