0a4ff4730f4e7dc8ac98594d25bc6bf9c2002b7b
Eduard Bondarenko <eduardbcom@gmail.com>
2017-09-06 18:30:38 +0300

    migrate code to classes

--- src/FramesReducer.js
+++ src/FramesReducer.js
@@ -8,93 +8,96 @@ const Errors = require('src/Errors');
 const startFrameTag = '[FRAME]';
 const endFrameTag   = '[/FRAME]';
 
-function createFramesReducer(config) {
-    let chunkRemainder = '';
+class FramesReducer extends EventEmitter {
+    constructor(config) {
+        super();
 
-    const ee = new EventEmitter();
+        if (!_.isPlainObject(config)) {
+            throw new TypeError('Config param should be a plain object.');
+        }
 
-    if (!_.isPlainObject(config)) {
-        throw new TypeError('Config param should be a plain object.');
+        if (!_.isInteger(config.bufferMaxLengthInBytes) || config.bufferMaxLengthInBytes < 0) {
+            throw new Errors.ConfigError('bufferMaxLengthInBytes param should be a positive integer.');
+        }
+
+        this._config         = config;
+        this._chunkRemainder = '';
     }
 
-    if (!_.isInteger(config.bufferMaxLengthInBytes) || config.bufferMaxLengthInBytes < 0) {
-        throw new Errors.ConfigError('bufferMaxLengthInBytes param should be a positive integer.');
-    }
+    process(newChunk) {
+        let frames = [];
 
-    return Object.assign(Object.create(ee), {
-        process(newChunk) {
-            let frames = [];
+        const data = this._chunkRemainder + newChunk;
 
-            const data = chunkRemainder + newChunk;
+        if (data.length > this._config.bufferMaxLengthInBytes) {
+            const error = new Errors.InvalidFrameError(
+                'Too long (probably infinite) frame.' +
+                `The frame length is ${data.length}.` +
+                `The max frame length must be ${this._config.bufferMaxLengthInBytes}`
+            );
 
-            if (data.length > config.bufferMaxLengthInBytes) {
-                const error = new Errors.InvalidFrameError(
-                    'Too long (probably infinite) frame.' +
-                    `The frame length is ${data.length}.` +
-                    `The max frame length must be ${config.bufferMaxLengthInBytes}`
-                );
+            return this.emit('error', error);
+        }
 
-                return this.emit('error', error);
-            }
+        try {
+            const res = FramesReducer.reduceFramesFromChunks(data);
 
-            try {
-                ({chunkRemainder, frames} = this._reduceFramesFromChunks(data));
-            } catch (error) {
-                return this.emit('error', error);
-            }
+            this._chunkRemainder = res.chunkRemainder;
+            frames               = res.frames;
+        } catch (error) {
+            return this.emit('error', error);
+        }
 
-            for (const frame of frames) {
-                this.emit('frame', this._frameToJson(frame));
-            }
-        },
-        reset() {
-            chunkRemainder = '';
-        },
-        _reduceFramesFromChunks,
-        _frameToJson
-    });
-}
-
-module.exports = createFramesReducer;
-
-function _reduceFramesFromChunks(data) {
-    let chunkRemainder = '';
-    let frames         = data.split(endFrameTag);
-
-    if (frames[frames.length - 1]) {
-        chunkRemainder = frames[frames.length - 1];
-    }
-
-    frames.splice(-1);
-
-    for (const frame of frames) {
-        if (
-            frame.indexOf(startFrameTag) === -1
-            || (frame.indexOf(startFrameTag) !== frame.lastIndexOf(startFrameTag))
-        ) {
-            throw new Errors.InvalidFrameError('Can not process frame with invalid structure.', {data, frame});
+        for (const frame of frames) {
+            this.emit('frame', FramesReducer.frameToJson(frame));
         }
     }
 
-    frames = frames.map(frame => frame.replace(startFrameTag, ''));
+    reset() {
+        this._chunkRemainder = '';
+    }
 
-    frames = frames.map(frame => frame.trim());
+    static reduceFramesFromChunks(data) {
+        let chunkRemainder = '';
+        let frames         = data.split(endFrameTag);
 
-    return {chunkRemainder, frames};
-}
-
-function _frameToJson(rawFrame) {
-    const frame      = {};
-    const frameLines = rawFrame.split('\n');
-
-    frameLines.forEach(frameLine => {
-        let [key, value] = frameLine.split('=').map(item => item.trim());
-
-        if (key && value) {
-            value      = Number(value) ? Number(value) : value;
-            frame[key] = value;
+        if (frames[frames.length - 1]) {
+            chunkRemainder = frames[frames.length - 1];
         }
-    });
 
-    return frame;
+        frames.splice(-1);
+
+        for (const frame of frames) {
+            if (
+                frame.indexOf(startFrameTag) === -1
+                || (frame.indexOf(startFrameTag) !== frame.lastIndexOf(startFrameTag))
+            ) {
+                throw new Errors.InvalidFrameError('Can not process frame with invalid structure.', {data, frame});
+            }
+        }
+
+        frames = frames.map(frame => frame.replace(startFrameTag, ''));
+
+        frames = frames.map(frame => frame.trim());
+
+        return {chunkRemainder, frames};
+    }
+
+    static frameToJson(rawFrame) {
+        const frame      = {};
+        const frameLines = rawFrame.split('\n');
+
+        frameLines.forEach(frameLine => {
+            let [key, value] = frameLine.split('=').map(item => item.trim());
+
+            if (key && value) {
+                value      = Number(value) ? Number(value) : value;
+                frame[key] = value;
+            }
+        });
+
+        return frame;
+    }
 }
+
+module.exports = FramesReducer;
--- tests/Unit/FramesReducer/constructor.test.js
+++ tests/Unit/FramesReducer/constructor.test.js
@@ -3,24 +3,24 @@
 const {assert}   = require('chai');
 const dataDriven = require('data-driven');
 
-const createFramesReducer = require('src/FramesReducer');
+const FramesReducer = require('src/FramesReducer');
 
 const Errors = require('src/Errors');
 
-const testData = require('./createFramesReducer.data');
+const testData = require('./constructor.data');
 
 function typeOf(item) {
     return Object.prototype.toString.call(item);
 }
 
-describe('FramesReducer::createFramesReducer', () => {
+describe('FramesReducer::constructor', () => {
 
     dataDriven(
         testData.incorrectConfig.map(item => ({type: typeOf(item), config: item})),
         () => {
             it('config param has invalid ({type}) type', ctx => {
                 assert.throws(() => {
-                    createFramesReducer(ctx.config);
+                    new FramesReducer(ctx.config);
                 }, TypeError, 'Config param should be a plain object.');
             });
         }
@@ -31,7 +31,7 @@ describe('FramesReducer::createFramesReducer', () => {
         () => {
             it('config.bufferMaxLengthInBytes param has invalid value ({type})', ctx => {
                 assert.throws(() => {
-                    createFramesReducer({bufferMaxLengthInBytes: ctx.value});
+                    new FramesReducer({bufferMaxLengthInBytes: ctx.value});
                 }, Errors.ConfigError, 'bufferMaxLengthInBytes param should be a positive integer.');
             });
         }
@@ -40,7 +40,7 @@ describe('FramesReducer::createFramesReducer', () => {
     dataDriven(testData.incorrectConfigValue, () => {
         it('{description}', ctx => {
             assert.throws(() => {
-                createFramesReducer(ctx.config);
+                new FramesReducer(ctx.config);
             }, Errors.ConfigError, ctx.errorMsg);
         });
     });
@@ -51,7 +51,7 @@ describe('FramesReducer::createFramesReducer', () => {
         };
 
         assert.doesNotThrow(() => {
-            createFramesReducer(correctConfig);
+            new FramesReducer(correctConfig);
         });
     });
 
