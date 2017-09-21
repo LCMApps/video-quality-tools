15949717144c6c489c4bf5c38f8db214a43589f3
Eduard Bondarenko <eduard.bondarenko@auroraglobal.com>
2017-09-21 14:00:15 +0300

    Return remain frames after process (refs #18372)

--- tests/Functional/processFrames.test.js
+++ tests/Functional/processFrames.test.js
@@ -1,7 +1,7 @@
 'use strict';
 
+const _       = require('lodash');
 const path    = require('path');
-const {spawn} = require('child_process');
 
 const {assert} = require('chai');
 const getPort  = require('get-port');
@@ -9,10 +9,7 @@ const getPort  = require('get-port');
 const FramesMonitor = require('src/FramesMonitor');
 const processFrames = require('src/processFrames');
 
-assert(process.env.FFPROBE, 'Specify path for ffprobe via FFPROBE env var');
-assert(process.env.FFMPEG, 'Specify path for ffmpeg via FFMPEG env var');
-
-const {FFPROBE, FFMPEG} = process.env;
+const {startStream} = require('./Helpers');
 
 const testFileWithFixedGop = path.join(__dirname, '../inputs/test_IPPPP.mp4');
 const testFileWithOpenGop  = path.join(__dirname, '../inputs/test_open_gop.mp4');
@@ -20,14 +17,13 @@ const testFileWithOpenGop  = path.join(__dirname, '../inputs/test_open_gop.mp4')
 const bufferMaxLengthInBytes = 2 ** 20;
 
 const config = {
-    ffprobePath           : FFPROBE,
+    ffprobePath           : process.env.FFPROBE,
     timeoutInSec          : 1,
     bufferMaxLengthInBytes: bufferMaxLengthInBytes
 };
 
 describe('processFrames functional tests', () => {
 
-    let ffmpeg;
     let framesMonitor;
     let streamUrl;
 
@@ -39,17 +35,7 @@ describe('processFrames functional tests', () => {
         framesMonitor = new FramesMonitor(config, streamUrl);
     });
 
-    afterEach(() => {
-        // at this stage ffmpeg process already finished
-        // but for sure let's kill it
-        ffmpeg.kill('SIGKILL');
-    });
-
-    it('must return correct frames info for the stream with fixed gop', done => {
-        let command = buildFfmpegCommand(testFileWithFixedGop, streamUrl);
-
-        ffmpeg = spawn(command[0], command.slice(1));
-
+    it('must return correct frames info for the stream with fixed gop', async () => {
         const frames = [];
 
         framesMonitor.on('stderr', chunk => {
@@ -64,47 +50,53 @@ describe('processFrames functional tests', () => {
             frames.push(frame);
         });
 
-        framesMonitor.on('exit', (code, signal) => {
-            const expectedExitCode = 0;
-            const expectedSignal   = null;
+        await startStream(testFileWithFixedGop, streamUrl);
 
-            const expectedMinFps  = 29.940119760479035;
-            const expectedMaxFps  = 30.120481927710856;
-            const expectedMeanFps = 30.001259478184732;
+        framesMonitor.listen();
 
-            const expectedMinBitrate  = 61.70471556886227;
-            const expectedMaxBitrate  = 3691.1709337349394;
-            const expectedMeanBitrate = 1018.2511085012945;
+        return new Promise(resolve => {
+            framesMonitor.on('exit', (code, signal) => {
+                const expectedExitCode = 0;
+                const expectedSignal   = null;
 
-            assert.strictEqual(code, expectedExitCode);
-            assert.strictEqual(signal, expectedSignal);
+                const expectedMinFps  = 29.940119760479035;
+                const expectedMaxFps  = 30.120481927710856;
+                const expectedMeanFps = 30.001259478184732;
 
-            const info = processFrames(frames);
+                const expectedMinBitrate  = 61.70471556886227;
+                const expectedMaxBitrate  = 3691.1709337349394;
+                const expectedMeanBitrate = 1018.2511085012945;
 
-            assert.isTrue(info.areAllGopsIdentical);
+                const expectedRemainedFrames = [
+                    {key_frame: 1, pict_type: 'I'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'}
+                ];
 
-            assert.strictEqual(info.fps.mean, expectedMeanFps);
-            assert.strictEqual(info.fps.min, expectedMinFps);
-            assert.strictEqual(info.fps.max, expectedMaxFps);
+                assert.strictEqual(code, expectedExitCode);
+                assert.strictEqual(signal, expectedSignal);
 
-            assert.strictEqual(info.bitrate.mean, expectedMeanBitrate);
-            assert.strictEqual(info.bitrate.min, expectedMinBitrate);
-            assert.strictEqual(info.bitrate.max, expectedMaxBitrate);
+                const {payload, remainedFrames} = processFrames(frames);
 
-            done();
-        });
+                assert.deepEqual(payload, {
+                    areAllGopsIdentical: true,
+                    fps                : {mean: expectedMeanFps, min: expectedMinFps, max: expectedMaxFps},
+                    bitrate            : {mean: expectedMeanBitrate, min: expectedMinBitrate, max: expectedMaxBitrate}
+                });
 
-        // rely on ffmpeg stderr output after the start
-        ffmpeg.stderr.once('data', () => {
-            framesMonitor.listen();
+                assert.deepEqual(
+                    remainedFrames.map(frame => _.pick(frame, ['key_frame', 'pict_type'])),
+                    expectedRemainedFrames
+                );
+
+                resolve();
+            });
         });
     });
 
-    it('must return correct frames info for the stream with open gop', done => {
-        let command = buildFfmpegCommand(testFileWithOpenGop, streamUrl);
-
-        ffmpeg = spawn(command[0], command.slice(1));
-
+    it('must return correct frames info for the stream with open gop', async () => {
         const frames = [];
 
         framesMonitor.on('stderr', chunk => {
@@ -119,44 +111,56 @@ describe('processFrames functional tests', () => {
             frames.push(frame);
         });
 
-        framesMonitor.on('exit', (code, signal) => {
-            const expectedExitCode = 0;
-            const expectedSignal   = null;
+        await startStream(testFileWithOpenGop, streamUrl);
 
-            const expectedMinFps  = 29.98696219035202;
-            const expectedMaxFps  = 30.000000000000004;
-            const expectedMeanFps = 29.995654063450672;
+        framesMonitor.listen();
 
-            const expectedMinBitrate  = 824.3019153225807;
-            const expectedMaxBitrate  = 1435.5312499999998;
-            const expectedMeanBitrate = 1066.2038272718032;
+        return new Promise(resolve => {
+            framesMonitor.on('exit', (code, signal) => {
+                const expectedExitCode = 0;
+                const expectedSignal   = null;
 
-            assert.strictEqual(code, expectedExitCode);
-            assert.strictEqual(signal, expectedSignal);
+                const expectedMinFps  = 29.98696219035202;
+                const expectedMaxFps  = 30.000000000000004;
+                const expectedMeanFps = 29.995654063450672;
 
-            const info = processFrames(frames);
+                const expectedMinBitrate  = 824.3019153225807;
+                const expectedMaxBitrate  = 1435.5312499999998;
+                const expectedMeanBitrate = 1066.2038272718032;
 
-            assert.isFalse(info.areAllGopsIdentical);
+                const expectedRemainedFrames = [
+                    {key_frame: 1, pict_type: 'I'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'},
+                    {key_frame: 0, pict_type: 'P'}
+                ];
 
-            assert.strictEqual(info.fps.mean, expectedMeanFps);
-            assert.strictEqual(info.fps.min, expectedMinFps);
-            assert.strictEqual(info.fps.max, expectedMaxFps);
+                assert.strictEqual(code, expectedExitCode);
+                assert.strictEqual(signal, expectedSignal);
 
-            assert.strictEqual(info.bitrate.mean, expectedMeanBitrate);
-            assert.strictEqual(info.bitrate.min, expectedMinBitrate);
-            assert.strictEqual(info.bitrate.max, expectedMaxBitrate);
+                const {payload, remainedFrames} = processFrames(frames);
 
-            done();
-        });
+                assert.deepEqual(payload, {
+                    areAllGopsIdentical: false,
+                    fps                : {mean: expectedMeanFps, min: expectedMinFps, max: expectedMaxFps},
+                    bitrate            : {mean: expectedMeanBitrate, min: expectedMinBitrate, max: expectedMaxBitrate}
+                });
 
-        // rely on ffmpeg stderr output after the start
-        ffmpeg.stderr.once('data', () => {
-            framesMonitor.listen();
+                assert.deepEqual(
+                    remainedFrames.map(frame => _.pick(frame, ['key_frame', 'pict_type'])),
+                    expectedRemainedFrames
+                );
+
+                resolve();
+            });
         });
     });
 
 });
-
-function buildFfmpegCommand(testFile, streamUrl) {
-    return `${FFMPEG} -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamUrl}`.split(' ');
-}
