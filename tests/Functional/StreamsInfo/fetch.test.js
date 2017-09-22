d7fcfb1f4d92d111cc2e6fa665e800c94b87406d
Eduard Bondarenko <eduard.bondarenko@auroraglobal.com>
2017-09-22 15:15:15 +0300

    Refactor tests in order to use helpers functions (refs #18375)

--- tests/Functional/StreamsInfo/fetch.test.js
+++ tests/Functional/StreamsInfo/fetch.test.js
@@ -1,41 +1,34 @@
 'use strict';
 
-const path    = require('path');
-const {spawn} = require('child_process');
+const path = require('path');
 
 const {assert} = require('chai');
+const getPort  = require('get-port');
 
-const getPort = require('get-port');
-
-const StreamsInfo = require('src/StreamsInfo');
-
+const StreamsInfo        = require('src/StreamsInfo');
 const {StreamsInfoError} = require('src/Errors');
 
-assert(process.env.FFPROBE, 'Specify path for ffprobe via FFPROBE env var');
-assert(process.env.FFMPEG, 'Specify path for ffmpeg via FFMPEG env var');
+const {startStream, stopStream} = require('../Helpers');
 
-const {FFPROBE, FFMPEG} = process.env;
-const testFile          = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
+const testFile = path.join(__dirname, '../../inputs/test_IPPPP.mp4');
 
 describe('StreamsInfo::fetch, fetch streams info from inactive stream', () => {
 
     let streamUrl;
     let streamsInfo;
 
-    before(async () => {
+    beforeEach(async () => {
         const port = await getPort();
 
         streamUrl = `http://localhost:${port}`;
 
         streamsInfo = new StreamsInfo({
-            ffprobePath : FFPROBE,
+            ffprobePath : process.env.FFPROBE,
             timeoutInSec: 1,
         }, streamUrl);
     });
 
-    it('fetch streams info from inactive stream', async function () {
-        this.timeout(5 * 1000);
-
+    it('fetch streams info from inactive stream', async () => {
         try {
             await streamsInfo.fetch();
 
@@ -51,65 +44,58 @@ describe('StreamsInfo::fetch, fetch streams info from inactive stream', () => {
 
 describe('StreamsInfo::fetch, fetch streams info from active stream', () => {
 
+    let stream;
     let streamUrl;
     let streamsInfo;
-    let ffmpeg;
 
-    before(async () => {
+    beforeEach(async () => {
         const port = await getPort();
 
         streamUrl = `http://localhost:${port}`;
 
         streamsInfo = new StreamsInfo({
-            ffprobePath : FFPROBE,
+            ffprobePath : process.env.FFPROBE,
             timeoutInSec: 1,
         }, streamUrl);
 
-        let command = `${FFMPEG} -re -i ${testFile} -vcodec copy -acodec copy -listen 1 -f flv ${streamUrl}`.split(' ');
-
-        ffmpeg = spawn(command[0], command.slice(1));
+        stream = await startStream(testFile, streamUrl);
     });
 
-    after(() => {
-        ffmpeg.kill('SIGKILL');
+    afterEach(async () => {
+        await stopStream(stream);
     });
 
-    it('fetch streams info from active stream', function (done) {
-        this.timeout(5 * 1000);
+    it('fetch streams info from active stream', async () => {
+        const info = await streamsInfo.fetch();
 
-        // we listen stderr cuz we rely on stderr banner output
-        ffmpeg.stderr.once('data', async () => {
-            const info = await streamsInfo.fetch();
+        assert.isObject(info);
 
-            assert.isObject(info);
+        const {videos, audios} = info;
 
-            const {videos, audios} = info;
+        assert.lengthOf(videos, 1);
+        assert.lengthOf(audios, 1);
 
-            assert.lengthOf(videos, 1);
-            assert.lengthOf(audios, 1);
-
-            assert.deepInclude(videos[0], {
-                codec_name          : 'h264',
-                codec_long_name     : 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10',
-                profile             : 'Main',
-                codec_type          : 'video',
-                width               : 854,
-                height              : 480,
-                sample_aspect_ratio : '1280:1281',
-                display_aspect_ratio: '16:9'
-            });
-
-            assert.deepInclude(audios[0], {
-                codec_name     : 'aac',
-                codec_long_name: 'AAC (Advanced Audio Coding)',
-                profile        : 'LC',
-                channels       : 6,
-                channel_layout : '5.1',
-                bit_rate       : '64775'
-            });
-
-            done();
+        assert.deepInclude(videos[0], {
+            codec_name          : 'h264',
+            codec_long_name     : 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10',
+            profile             : 'Main',
+            codec_type          : 'video',
+            width               : 854,
+            height              : 480,
+            sample_aspect_ratio : '1280:1281',
+            display_aspect_ratio: '16:9'
         });
+
+        assert.deepInclude(audios[0], {
+            codec_name     : 'aac',
+            codec_long_name: 'AAC (Advanced Audio Coding)',
+            profile        : 'LC',
+            channels       : 6,
+            channel_layout : '5.1',
+            bit_rate       : '64775'
+        });
+
+        return Promise.resolve();
     });
 
 });
