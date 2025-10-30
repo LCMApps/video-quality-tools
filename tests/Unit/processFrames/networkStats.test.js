4ef0b9483bd15e30a288c5137df5c38e22b5f4f3
Dmytro Menshykov <d.menshikov@gmail.com>
2025-10-30 13:30:36 +0100

    Update networkStats.test.js to use VideoFrameSchema1 and AudioFrameSchema1 (refs #119)

--- tests/Unit/processFrames/networkStats.test.js
+++ tests/Unit/processFrames/networkStats.test.js
@@ -6,7 +6,7 @@ const dataDriven = require('data-driven');
 
 const processFrames = require('src/processFrames');
 
-const {invalidFramesTypes, invalidDurationInSecTypes, testData} = require('./networkStats.data');
+const {invalidFramesTypes, invalidDurationInSecTypes, testData, testDataWithValueObjects} = require('./networkStats.data');
 
 const PRECISION = 0.00001;
 
@@ -14,7 +14,7 @@ function typeOf(item) {
     return Object.prototype.toString.call(item);
 }
 
-describe('processFrames.networkStats', () => {
+describe('processFrames.networkStats - raw frame object', () => {
 
     dataDriven(
         invalidFramesTypes.map(item => ({type: typeOf(item), item: item})),
@@ -39,6 +39,63 @@ describe('processFrames.networkStats', () => {
     );
 
     dataDriven(testData, () => {
+        it('{description}', ctx => {
+            const expectedResult = ctx.expected;
+
+            const result = processFrames.networkStats(ctx.frames, ctx.durationInMsec);
+
+            assert.isTrue(_.inRange(
+                result.videoFrameRate,
+                expectedResult.videoFrameRate - PRECISION,
+                expectedResult.videoFrameRate + PRECISION,
+            ));
+
+            assert.isTrue(_.inRange(
+                result.audioFrameRate,
+                expectedResult.audioFrameRate - PRECISION,
+                expectedResult.audioFrameRate + PRECISION,
+            ));
+
+            assert.isTrue(_.inRange(
+                result.videoBitrate,
+                expectedResult.videoBitrate - PRECISION,
+                expectedResult.videoBitrate + PRECISION
+            ));
+
+            assert.isTrue(_.inRange(
+                result.audioBitrate,
+                expectedResult.audioBitrate - PRECISION,
+                expectedResult.audioBitrate + PRECISION
+            ));
+        });
+    });
+});
+
+describe('processFrames.networkStats - frame value objects', () => {
+
+    dataDriven(
+        invalidFramesTypes.map(item => ({type: typeOf(item), item: item})),
+        () => {
+            it('must throw an exception for invalid input {type} type for frames', ctx => {
+                assert.throws(() => {
+                    processFrames.networkStats(ctx.item);
+                }, TypeError, 'Method accepts only an array of frames');
+            });
+        }
+    );
+
+    dataDriven(
+        invalidDurationInSecTypes.map(item => ({type: typeOf(item), item: item})),
+        () => {
+            it('must throw an exception for invalid input {type} type for durationInMsec', ctx => {
+                assert.throws(() => {
+                    processFrames.networkStats([], ctx.item);
+                }, TypeError, 'Method accepts only a positive integer as duration');
+            });
+        }
+    );
+
+    dataDriven(testDataWithValueObjects, () => {
         it('{description}', ctx => {
             const expectedResult = ctx.expected;
 
